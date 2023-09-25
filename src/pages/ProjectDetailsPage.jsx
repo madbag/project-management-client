444ec52dd7f785db150ae162afd7ab11a73d3309
Madhushree B <boylemadhushree@gmail.com>
2023-09-25 20:00:57 +0200

    project-management-client

---
+++ src/pages/ProjectDetailsPage.jsx
@@ -0,0 +1,60 @@
+// src/pages/ProjectDetailsPage.jsx
+
+import { useState, useEffect } from "react";
+import axios from "axios";
+// import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import TaskCard from "../components/TaskCard";
+import AddTask from "../components/AddTask";
+
+const API_URL = "http://localhost:5005";
+
+function ProjectDetailsPage(props) {
+  const [project, setProject] = useState(null);
+  //get the URL parameter ':projectId'
+  const { projectId } = useParams();
+
+  // Helper function that makes a GET request to the API
+  // and retrieves the project by id
+  const getProject = () => {
+    axios
+      .get(`${API_URL}/api/projects/${projectId}`)
+      .then((response) => {
+        const oneProject = response.data;
+        setProject(oneProject);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    getProject();
+  }, []);
+
+  return (
+    <div className="ProjectDetails">
+      {project && (
+        <>
+          <h1>{project.title}</h1>
+          <p>{project.description}</p>
+        </>
+      )}
+
+      <AddTask refreshProject={getProject} projectId={projectId} />
+      
+      {project &&
+        project.tasks.map((task) => (
+          <TaskCard key={task._id} {...task}/>
+        ))}
+
+      <Link to="/projects">
+        <button>Back to projects</button>
+      </Link>
+
+      <Link to={`/projects/edit/${projectId}`}>
+        <button>Edit Project</button>
+      </Link>
+    </div>
+  );
+}
+
+export default ProjectDetailsPage;
